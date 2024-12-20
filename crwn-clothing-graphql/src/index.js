08a72addebcfa7ba77087e8177028423d549f663
Basim Dar <basimdar2002@hotmail.com>
2024-12-20 12:03:16 +0300

    Adding 'src' folder of sample React app using GraphQL/Apollo

---
+++ crwn-clothing-graphql/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+import App from './App';
+import { UserProvider } from './contexts/user.context';
+import { CartProvider } from './contexts/cart.context';
+import { CategoriesProvider } from './contexts/categories.context';
+
+import './index.scss';
+
+const rootElement = document.getElementById('root');
+
+const client = new ApolloClient({
+    uri: 'https://crwn-clothing.com/',
+    cache: new InMemoryCache(),
+  }
+);
+
+render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode>,
+  rootElement
+);
